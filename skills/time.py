a2e250ef5e676a3f4a86f7e41bf2239a6b37950a
NikaDe7 <98489029+NikaDe7@users.noreply.github.com>
2022-05-22 23:16:45 +0300

    Delete skills directory

--- skills/time.py
+++
@@ -1,9 +0,0 @@
-from package.skills.core import speak
-import datetime
-
-def Time():
-    strTime = datetime.datetime.now().strftime('%H:%M')
-    speak.Speak('Зараз')
-    speak.Speak(strTime)
-    print(strTime)
-
